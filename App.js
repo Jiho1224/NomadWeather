d52ced7322454f39f64a834d2d897ab75b7103e6
82103 <pjiho1224@gmail.com>
2022-08-14 18:12:22 +0900

    Add weather icon and date

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import {View, StyleSheet,Text,ScrollView, Dimensions
 ,ActivityIndicator } from 'react-native';
 import * as Location from 'expo-location';
-import { MaterialCommunityIcons, Entypo,FontAwesome } from '@expo/vector-icons';
+import { MaterialCommunityIcons, Entypo } from '@expo/vector-icons';
 
 
 const {width : SCREEN_WIDTH} = Dimensions.get("window");
@@ -19,6 +19,26 @@ export default function App() {
 
   const API_KEY="95170051ba82f5306bcacf94821f4ab7";
 
+  const icons={
+    "Clouds": "weather-cloudy",
+    "Thunderstorm":"weather-lightning",
+    "Drizzle":"weather-rainy",
+    "Rain":"weather-pouring",
+    "Snow":"weather-snowy-heavy",
+    "Mist":"weather-fog",
+    "Smoke":"weather-fog",
+    "Haze":"weather-hazy",
+    "Dust":"weather-fog",
+    "Fog":"weather-fog",
+    "Sand":"weather-fog",
+    "Ash":"weather-fog",
+    "Squall":"weather-windy-variant",
+    "Tornado":"weather-tornado",
+    "Clear":"weather-sunny",
+
+  }
+  
+  
   
   const ask = async()=>{
     const {granted} = await Location.requestForegroundPermissionsAsync();
@@ -50,6 +70,11 @@ export default function App() {
     ask();
   },[]);
 
+  let day = new Date();
+  let year = day.getFullYear();
+  let month = day.getMonth() + 1;
+  let date = day.getDate();
+
   return (
     <View style={style.container}>
       <StatusBar style="inverted"></StatusBar>
@@ -77,13 +102,20 @@ export default function App() {
              
              : (days.map((day,index) => 
              <View key={index} style={style.day}>
-                <Text style={style.temp}> {parseFloat(day.temp.day).toFixed(1)}</Text>
-                <Text style={style.des}>{day.weather[0].main}</Text>
+                {index == 0 ? (<Text style={style.date}>{year}.{month}.{date}</Text>
+                  ):
+                  (<Text style={style.date}>{index} day(s) after</Text>)}
+                <Text style={style.temp}> {parseFloat(day.temp.day).toFixed(1)}<Text style={{fontSize:30}}>°C</Text></Text>
+                <View style={{flexDirection:"row"}}>
+                  <MaterialCommunityIcons style={style.weatherIcon} name={icons[day.weather[0].main]} size={30} color="white" />
+                  <Text style={style.des}>{day.weather[0].main}</Text>
+                </View>
+                
              </View>)
              )
           }
 
-
+          {/* Clear Clouds Rain  */}
 
       </ScrollView>
 
@@ -125,13 +157,20 @@ const style = StyleSheet.create({
 
   day:{
     width:SCREEN_WIDTH,
-    alignItems:"center",
+    // alignItems:"left",
+  },
+  date:{
+    marginTop:90,
+    marginLeft:40,
+    fontSize:20,
+    color:"white",
+    fontWeight:"500",
   },
 
   temp:{
-    marginTop:50,
+    marginTop:-20,
     fontSize: 120,
-    textAlign:"center",
+    
     fontWeight:"400",
     color: "white",
     paddingRight:30,
@@ -139,9 +178,17 @@ const style = StyleSheet.create({
 
   des:{
     marginTop:-30,
-    fontSize:50,
+    fontSize:40,
+    marginLeft:10,
     color: "white",
   },
+
+  weatherIcon:{
+    marginTop:-15,
+    marginLeft:35,
+  },
+
+  
 });
 
 
